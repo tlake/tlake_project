d95dd3367a54b42dc588c0803442e13eea00c627
tlake <tanner.lake@gmail.com>
2015-07-28 10:54:17 -0700

    added date field for project models

--- app_projects/models.py
+++ app_projects/models.py
@@ -7,6 +7,7 @@ class Project(models.Model):
     title = models.CharField(max_length=127)
     desc = models.TextField()
     link = models.URLField(max_length=255)
+    date_created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return "Project: {}".format(self.title)
