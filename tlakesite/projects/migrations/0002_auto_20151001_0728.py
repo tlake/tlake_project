decafb550502673f4b8a01066605b236ac1103ea
tlake <tanner.lake@gmail.com>
2015-10-01 00:36:41 -0700

    migrations

---
+++ tlakesite/projects/migrations/0002_auto_20151001_0728.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.utils.timezone
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('projects', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='project',
+            name='date_created',
+            field=models.DateTimeField(default=django.utils.timezone.now),
+        ),
+    ]
