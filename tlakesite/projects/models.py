db5fe4f9f14c89ab6f6d5b03c5f72e5b205c7915
tlake <tanner.lake@gmail.com>
2015-10-01 00:53:50 -0700

    projects now have links for both site and source code

--- tlakesite/projects/models.py
+++ tlakesite/projects/models.py
@@ -7,7 +7,8 @@ from django.utils import timezone
 class Project(models.Model):
     title = models.CharField(max_length=127)
     desc = models.TextField()
-    link = models.URLField(max_length=255)
+    site_link = models.URLField(max_length=255, null=True, blank=True)
+    code_link = models.URLField(max_length=255, null=True, blank=True)
     date_created = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
